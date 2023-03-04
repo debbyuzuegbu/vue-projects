0c3d3bcfe72a2a8a4252c782a08bb6d5c34a9061
Deborah <debbyuzuegbu@gmail.com>
2023-03-04 20:16:54 +0100

    added files

---
+++ src/components/FriendComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "manuel",
+        name: "manuel lionel",
+        phone: "0134 67945 45",
+        email: "manuel@gmail.com",
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import FriendComponent from "./components/FriendComponent.vue"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.component('friend-contact', FriendComponent);
+
+app.mount('#app')
